00d62c6d684419ef1450f42e1bcbfbb24c2f5760
Alyssa Rodriguez <alyssa.taylor.rodriguez@gmail.com>
2022-03-24 23:33:45 -0500

    Script file created.

---
+++ Assets/js/script.js
@@ -0,0 +1,12 @@
+
+// Here I created functions so that when you click on a project, it takes you to the project website.
+
+$("#card1").on("click", function(){
+    window.open("https://eugene125.github.io/project-1/")
+})
+$("#card2").on("click", function(){
+    window.open("https://alyssarodri.github.io/day-planner/")
+})
+$("#card3").on("click", function(){
+    window.open("https://alyssarodri.github.io/web-APIs-code-quiz/")
+})
